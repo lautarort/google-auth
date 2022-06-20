f70773141fb42b812053b57bacc26d166deac100
unknown <lautarorangilt@gmail.com>
2022-06-19 22:17:50 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -4,15 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* <button onClick={signInWithGoogle}>Sign In With Google</button> */}
-
       <button type="button" class="login-with-google-btn" onClick={signInWithGoogle} >
         Sign in with Google
       </button>
-
-      {/* <button type="button" class="login-with-google-btn" disabled onClick={signInWithGoogle}>
-        Sign in with Google
-      </button> */}
 
 
       <h1>{localStorage.getItem("name")}</h1>
